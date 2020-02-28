8f32cdf0464569e95df416eaf191e05d5a40a7a5
Mateusz <chomamateusz@gmail.com>
2020-02-28 13:15:32 +0100

    Automatic commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Primitives
+
+// Number
+5
+1.5
+// String
+'Hello'
+"I can't"
+// Boolean
+true
+false
+// null
+null
+// undefined
+undefined
+
+// Objects
+
+// Object
+;({
+    name: 'Fluffy',
+    age: 3
+})
+// Array (subtype or kind of Object)
+[ 'Fluffy', 3 ]
+// Functions are also Objects !!!
